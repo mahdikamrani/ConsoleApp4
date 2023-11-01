302a993977792b5cc79e77266b3f35b9167db1bd
owner <owner@DESKTOP-4O1GNO6>
2023-11-01 08:33:51 +0330

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {long recursivefact = recursivefactorial(10);
+            Console.WriteLine(recursivefact);
+            long loopfact = loopfactorial(20);
+            Console.WriteLine(loopfact);
+            BigInteger bigfact = Bigfactorial(30);
+            Console.WriteLine(bigfact);
+            Console.ReadKey();
+        }// مهدی کامرانی نژاد
+    }
+    }
+}
